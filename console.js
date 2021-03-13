b46ac9c032359c43dc78ef9e1fdad52e0709b61b
PRANAV TRIPATHI <76243022+PranavTrip@users.noreply.github.com>
2021-03-13 23:57:40 +0530

    TYPES OF CONSOLE IN JS

---
+++ console.js
@@ -0,0 +1,19 @@
+// JavaScript supports many type of consoles like : console.log/table/warn/error/assert/clear/time/count etc.
+// Console.time
+console.time();
+// Console.log
+console.log("This is console.log");
+// Console.table
+let obj1 = { name: "Pranav", age: 19, role: "None" };
+console.table(obj1);
+// Console.assert
+console.assert(0 > 1, "False");
+// Console.warn
+console.warn("This is a warning");
+// Console.error
+console.error("This is an error");
+// Console.clear : Used to clear console.
+// console.clear();
+
+// Console.timeEnd
+console.timeEnd();
