f7cf5953d873923397eb5b7931e79d11f2938f77
PRANAV TRIPATHI <76243022+PranavTrip@users.noreply.github.com>
2021-03-13 23:58:24 +0530

    TYPE CONVERSION OF VARIABLES IN JS

---
+++ type-conversion.js
@@ -0,0 +1,6 @@
+// In JavaScript type of variables can be changed by using the type conversion and type coercion properties
+let num = 221;
+console.log("The type of num is " + typeof num);
+
+let numNew=String(num);
+console.log("The  type of num after type conversion is " + typeof numNew);
