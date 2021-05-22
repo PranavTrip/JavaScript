84c3a5bfec26fb3c9586d8b3bb18ab93faca18f5
PRANAV TRIPATHI <76243022+PranavTrip@users.noreply.github.com>
2021-05-22 11:45:13 +0530

    Classes in JavaScript

---
+++ classes.js
@@ -0,0 +1,13 @@
+// Classes in JavaScript
+// Classes in JS are made with the help of constructors and are very similar to Objects.
+class simpleDate {
+  constructor(year, month, day) {
+    this.year = year;
+    this.month = month;
+    this.day = day;
+  }
+  // A class can have as many functions
+  goodMorning() {
+    return "Good Morning";
+  }
+}
