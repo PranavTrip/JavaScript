0fbc9de57346dfa2027949f6c93a63c55ebdb591
PRANAV TRIPATHI <76243022+PranavTrip@users.noreply.github.com>
2021-05-22 20:34:38 +0530

    Symbols in JS

---
+++ symbols.js
@@ -0,0 +1,13 @@
+// Symbols in JavaScript
+
+// Defining Symbol 1
+const sym1 = Symbol();
+
+// Defining Symbol 2
+const sym2 = Symbol();
+
+// Checking whether the symbols are equal
+console.log(
+  "Both 'sym1' and 'sym2' are undefined but they are not equal because symbols are always unique"
+);
+console.log(sym1 === sym2);
